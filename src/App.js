1326e3e5a1eb9772ffa02d0cdf40007610308500
ad551 <gdaim551@gmail.com>
2022-03-11 01:52:00 +0500

    earliest commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from './AppBar'
+import Resume from './Resume.js'
+
+const App = () => {
+  return (
+    <>
+      <AppBar/>
+      <Resume/>
+    </>
+  )
+}
+
+export default App
