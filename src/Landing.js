62de14cdcabf2fbf8935e303216662d71588b8cf
ad551 <gdaim551@gmail.com>
2022-06-23 21:10:23 +0500

    bg

--- src/Landing.js
+++ src/Landing.js
@@ -4,10 +4,12 @@ import './Landing.css'
 const Landing = () => {
   return (
     <>
-        <div id="div1" style={{marginTop:"16vh", marginLeft:"1%"}}>UI / UX Researcher</div>
-        <div id="div2" style={{margin:"1%"}}>Web Designer</div>
-        <div id="div3" style={{margin:"1%"}}>Web Developer</div>
-        <div id="div4" style={{margin:"1%"}}>Software Engineer</div>
+    <div id="landing-bg">
+      <div id="div1" style={{marginTop:"16vh", marginLeft:"1%"}}>UI / UX Researcher</div>
+      <div id="div2" style={{margin:"1%"}}>Web Designer</div>
+      <div id="div3" style={{margin:"1%"}}>Web Developer</div>
+      <div id="div4" style={{margin:"1%"}}>Software Engineer</div>
+    </div>
     </>
   )
 }
